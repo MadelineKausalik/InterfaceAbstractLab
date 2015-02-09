a0d4736949088e4a35f50e5cb747832e8ef19ddd
mkausalik3 <mkausalik3@C02017042748.wctc.msft>
2015-02-09 17:16:35 -0600

    Deleted code from: AdvancedJavaCourse IntroJavaCourse IntroToProgramming
    Because it is in CollegeCourse

---
+++ src/lab1/CollegeCourse.java
@@ -0,0 +1,77 @@
+package lab1;
+
+import javax.swing.JOptionPane;
+
+
+public abstract class CollegeCourse {
+    
+    //This is the abstract super class
+    
+    private String courseName;
+    private String courseNumber;
+    private double credits;
+    private String prerequisites;
+    
+    public void IntroJavaCourse(String courseName, String courseNumber) {
+        this.courseName = courseName;
+        this.courseNumber = courseNumber;
+    }
+    
+    public void IntroToProgrammingCourse(String courseName, String courseNumber) {
+        this.setCourseName(courseName);
+        this.setCourseNumber(courseNumber);
+    }
+
+    public void AdvancedJavaCourse(String courseName, String courseNumber) {
+        this.setCourseName(courseName);
+        this.setCourseNumber(courseNumber);
+    }
+    
+    public String getCapitalizedCourseName() {
+        return this.getCourseName().toUpperCase();
+    }
+    
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(String courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+
+    public double getCredits() {
+        return credits;
+    }
+
+    public void setCredits(double credits) {
+        if(credits < 0 || credits > 5.0) {
+            System.out.println(
+                    "Error: credits must be in the range 0.5 to 4.0");
+            System.exit(0);
+        }
+        this.setCredits(credits);
+    }
+
+    public String getPrerequisites() {
+        return prerequisites;
+    }
+
+    public void setPrerequisites(String prerequisites) {
+        if(prerequisites == null || prerequisites.length() == 0) {
+            JOptionPane.showMessageDialog(null,
+                    "Error: prerequisites cannot be null of empty string");
+            System.exit(0);
+        }
+        this.prerequisites = prerequisites;
+        this.prerequisites = prerequisites;
+    }
+    
+}
